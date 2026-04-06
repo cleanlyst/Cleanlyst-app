7a24f74e4818f5699fb378cfb6b5b3cb8facf062
Brendan <ugobrendan@gmail.com>
2026-04-06 15:36:05 +0100

    Created styles pages and imported company logo

--- cleanlyst-frontend/src/main.ts
+++ cleanlyst-frontend/src/main.ts
@@ -4,6 +4,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import '../src/styles/layout.css'
+import '../src/styles/typography.css'
+import '../src/styles/styles.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
